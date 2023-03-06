da71466d85f1ea3d75bae2457dd86444b968977c
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2023-03-06 06:28:29 +0530

    Added Meta tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,18 @@
   import PageHero from "$lib/components/PageHero.svelte";
   import PageProd from "$lib/components/PageProd.svelte";
   import PageStats from "$lib/components/PageStats.svelte";
+  import Logo from "$lib/wristwatch.png";
 </script>
 
 <svelte:head>
   <title>Funky Tunky | Home Page</title>
+  <link rel="shortcut icon" href={Logo} type="image/x-icon" />
+  <meta name="description" content="Building Character Takes Time - Matrix" />
+  <meta
+    name="og:image"
+    content="https://cdn-icons-png.flaticon.com/512/9335/9335046.png"
+  />
+  <meta name="keywords" content="Watch,Matrix,Smart Watch" />
 </svelte:head>
 <main>
   <PageHero />
